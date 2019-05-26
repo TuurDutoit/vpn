e9de57f2d3b1d4a2b2c7f26051663a94e15712d5
Tuur Dutoit <me@tuurdutoit.be>
2019-05-26 14:34:14 +0200

    Fix routing

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -5,13 +5,15 @@ sed -i -E 's;#?\s*net\.ipv4\.ip_forward\s*=\s*[01];net.ipv4.ip_forward = 1;' /et
 sysctl -p /etc/sysctl.conf
 
 # Insert NAT rules into iptables via rc.local (executed when the server boots)
+# FORWARD rules target LOCAL_PRIVATE_IP as destination, because of the PREROUTING DNAT rule
+# SNAT happens in POSTROUTING, so destination is the real one
 echo "
 #!/bin/bash
 
 # Firewall & NAT rules for OpenVPN gateway to $CLIENT_NAME
 iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -s $LOCAL_PRIVATE_IP -j ACCEPT
-iptables -A FORWARD -d $REMOTE_PUBLIC_IP -j ACCEPT
+iptables -A FORWARD -d $LOCAL_PRIVATE_IP -j ACCEPT
 iptables -P FORWARD DROP
 iptables -t nat -A PREROUTING -i $REMOTE_PUBLIC_INT -d $REMOTE_PUBLIC_IP -j DNAT --to-destination $LOCAL_PRIVATE_IP
 iptables -t nat -A POSTROUTING -o $REMOTE_PUBLIC_INT -s $LOCAL_PRIVATE_IP -j SNAT --to-source $REMOTE_PUBLIC_IP
